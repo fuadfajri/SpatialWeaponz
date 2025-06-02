8c9c944aa51e6c425fcaeb4c8d9500073b399d6f
fuadfajri <fuadfajri@users.noreply.github.com>
2025-06-02 18:10:10 +0800

    Initial Commit

---
+++ SpatialWeaponz/SpatialWeaponzApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpatialWeaponzApp.swift
+//  SpatialWeaponz
+//
+//  Created by Fuad on 02/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SpatialWeaponzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpatialWeaponzTests/SpatialWeaponzTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpatialWeaponzTests.swift
+//  SpatialWeaponzTests
+//
+//  Created by Fuad on 02/06/25.
+//
+
+import Testing
+@testable import SpatialWeaponz
+
+struct SpatialWeaponzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
